97eebbf408e112eea5da06c49711b15188ed6058
Josiah Nieves <23145687+jtn7@users.noreply.github.com>
2020-05-06 13:33:48 -0400

    Updates

---
+++ precache-manifest.3e5f293f61715449a3c6b4d8fd3caa08.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d59cfb740e7396e9856d9035c6b11cc5",
+    "url": "/is322-102-project-2/index.html"
+  },
+  {
+    "revision": "5cd6e0c83720e9092dc8",
+    "url": "/is322-102-project-2/static/css/main.4d13f7da.chunk.css"
+  },
+  {
+    "revision": "da5568e6bb8c863d8f14",
+    "url": "/is322-102-project-2/static/js/2.181a8c58.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/is322-102-project-2/static/js/2.181a8c58.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5cd6e0c83720e9092dc8",
+    "url": "/is322-102-project-2/static/js/main.3a03e2da.chunk.js"
+  },
+  {
+    "revision": "fdc3a96e29c02291d39f",
+    "url": "/is322-102-project-2/static/js/runtime-main.3b46ab35.js"
+  }
+]);
